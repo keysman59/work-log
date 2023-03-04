f4775be991d94af608a592b28cc62aced23257b3
unknown <dima>
2023-03-04 19:00:16 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app" class="app">
+    <Table></Table>
+  </div>
+</template>
+
+<script>
+import Table from './components/Table'
+
+
+export default {
+  components: { Table },
+  data() {
+    return {
+
+    }
+  },
+}
+
+</script>
+
+
+<style>
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.app {
+
+}
+
+body {
+  background: #eee;
+}
+
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="table">
+        <div class="table-title">
+            <div class="table-title__text table-title__transition">Переход</div>
+            <div class="table-title__text table-title__status">Статус</div>
+            <div class="table-title__text table-title__start">Начало</div>
+            <div class="table-title__text table-title__end">Окончание</div>
+            <div class="table-title__text table-title__total">Всего</div>
+        </div>     
+        <div class="table-header">
+            Иванов Иван Иванович 
+        </div>     
+        <div class="table-body">
+            <div class="table-title__text table-title__transition">
+                
+            </div>
+            <div class="table-title__text table-title__status">Статус</div>
+            <div class="table-title__text table-title__start">Начало</div>
+            <div class="table-title__text table-title__end">Окончание</div>
+            <div class="table-title__text table-title__total">Всего</div>
+        </div>     
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Home',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+.table {
+    background: white;
+    padding: 19px 20px;
+}
+
+.table-title, .table-body {
+    display: flex;
+}
+
+.table-title__text {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
+
+.table-title__transition {
+    max-width: 248px;
+}
+
+.table-header {
+    padding-top: 12px;
+    padding-bottom: 14px;
+    text-align: center;
+}
+
+</style>
