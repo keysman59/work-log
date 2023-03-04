c1524d0f258aabc0c7a77aa48b0ad70710b65a0a
unknown <dima>
2023-03-04 19:13:50 +0300

    add local

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <div id="app" class="app">
     <Table></Table>
+    {{  items  }}
   </div>
 </template>
 
 <script>
 import Table from './components/Table'
-
+import { localItems } from './data/localData.js'
 
 export default {
   components: { Table },
   data() {
     return {
-
+      items: localItems
     }
   },
 }
